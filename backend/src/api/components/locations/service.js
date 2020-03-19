fc4ad506f67823b173c1b0c01a3ba99f428e3c3d
jltimm <jacobltimm@gmail.com>
2020-03-19 18:54:03 -0500

    adding-body-parser

---
+++ backend/src/api/components/locations/service.js
@@ -0,0 +1,13 @@
+module.exports = {getAverage};
+
+/**
+ * Averages the coordinates
+ *
+ * @param {array} locations Grabs the averaged coordinate
+ * @param {function} callback The callback
+ */
+function getAverage(locations, callback) {
+  // TODO: Actually implement
+  console.log(locations);
+  callback(null, locations[0]);
+}
--- backend/src/api/meetAt.js
+++ backend/src/api/meetAt.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use('/v1/locations', require('./components/locations/controller'));
 
 app.listen(process.env.PORT || 8081);

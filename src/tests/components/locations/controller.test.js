20ad4f111ee8bc4d7d39f745c2c2331eba8cb42c
jltimm <36246250+jltimm@users.noreply.github.com>
2020-03-22 17:10:09 -0500

    Adding tests for 400s (#17)

--- src/tests/components/locations/controller.test.js
+++ src/tests/components/locations/controller.test.js
@@ -98,6 +98,76 @@ describe('/v1/locations/center test, five different locations provided', () => {
   });
 });
 
+describe('/v1/locations/center test, body doesn\'t contain locations', () => {
+  it('should return 400 with the no locations error', async () => {
+    const res = await global.agent
+        .get('/v1/locations/center')
+        .send({
+          location: [{
+            lat: 44.9778,
+            lng: -93.2650,
+          }, {
+            lat: 44.9537,
+            lng: -93.0900,
+          }],
+        });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({
+      err: 'There was a problem validating the request body.',
+      msg: [
+        'There are no locations in the request body',
+      ],
+    });
+  });
+});
+
+describe('/v1/locations/center test, body contains a host of issues', () => {
+  it('should return 400 with the list of issues', async () => {
+    const res = await global.agent
+        .get('/v1/locations/center')
+        .send({
+          locations: [{
+            lat: 90.0001,
+            lng: -45.0000,
+          }, {
+            lat: -90.0001,
+            lng: -45.0000,
+          }, {
+            lat: 89.3445,
+            lng: 180.0001,
+          }, {
+            lat: 89.3445,
+            lng: -180.1111,
+          }, {
+            lt: 44.9537,
+            lg: -93.0900,
+          }, {
+            lat: 44.9537,
+            lng: -93.0900,
+          }, {
+            lat: 44.9537,
+            lg: -93.0900,
+          }, {
+            lt: 44.9537,
+            lng: -93.0900,
+          }],
+        });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({
+      err: 'There was a problem validating the request body.',
+      msg: [
+        'The coordinates at index 0 are not valid.',
+        'The coordinates at index 1 are not valid.',
+        'The coordinates at index 2 are not valid.',
+        'The coordinates at index 3 are not valid.',
+        'The lat and lng at index 4 are not present',
+        'The lng at index 6 is not present.',
+        'The lat at index 7 is not present.',
+      ],
+    });
+  });
+});
+
 afterEach(async () => {
   await server.close();
 });
